eb7ca98b391baa0fd430206129bdcade55569c6f
yura <yura.gusak7@gmail.com>
2016-12-04 23:45:48 +0200

    added view

---
+++ PiratesBayv.1.0/PiratesBayv.1.0.1/Pirates Bay/Command.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Pirates_Bay
+{
+    class Command:ICommand
+    {
+        public Func<bool> CanExecuteDelegate { get; set; }
+        public Action ExecuteDelegate { get; set; }
+        public Command(Func<bool> canExecute, Action execute)
+        {
+            CanExecuteDelegate = canExecute;
+            ExecuteDelegate = execute;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return CanExecuteDelegate != null ? CanExecuteDelegate() : true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            ExecuteDelegate();
+        }
+    }
+}
---
+++ PiratesBayv.1.0/PiratesBayv.1.0.1/Pirates Bay/Views/StatisticsWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Pirates_Bay.Views
+{
+    /// <summary>
+    /// Interaction logic for StatisticsWindow.xaml
+    /// </summary>
+    public partial class StatisticsWindow : Window
+    {
+        public StatisticsWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
